a3b1f711aa7b2512fbe8d1f7b44e4d92ef4590d0
semiya <jun_you@live.jp>
2012-11-05 22:07:21 +0900

    test code is refactored

--- queen_spec.rb
+++ queen_spec.rb
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 require_relative 'queen'
 describe "N-Queen"	do
+	before(:each) do
+		@queen = Queen.new
+	end
 	describe "N = 1 のとき"	do
-		before(:each) do
-			@queen = Queen.new
+		before do
 			@queen.try(1)
 		end
 		it "クラス名はあっているか？" do

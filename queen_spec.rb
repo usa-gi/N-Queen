607ce827775c77e4e16b6f6149f840621966a509
semiya <jun_you@live.jp>
2012-11-05 21:27:53 +0900

    first red

--- queen_spec.rb
+++ queen_spec.rb
@@ -1,5 +1,10 @@
 # -*- encoding: utf-8 -*-
-require_relative 'aiming'
+require_relative 'queen'
 describe "N-Queen"	do
-
+	describe "N = 1 のとき"	do
+		it "開始できるか？" do
+			queen = Queen.new
+		
+		end
+	end
 end

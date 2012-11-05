ac5a5c43fc90682fc2ae2f292ae7115f83d25834
semiya <jun_you@live.jp>
2012-11-05 22:14:30 +0900

    initialize method is refactored

--- queen_spec.rb
+++ queen_spec.rb
@@ -1,18 +1,18 @@
 # -*- encoding: utf-8 -*-
 require_relative 'queen'
 describe "N-Queen"	do
-	before(:each) do
-		@queen = Queen.new(1)
-	end
 	describe "N = 1 のとき"	do
-		before do
-			@queen.try(1)
+		before(:each) do
+			@queen = Queen.new(1)
 		end
-		it "クラス名はあっているか？" do
-			@queen.class.should == Queen
+		describe "初期処理のとき"	do
+			it "クラス名はあっているか？" do
+				@queen.class.should == Queen
+			end
+			it "盤の配置（Board）は初期化されているか？" do
+				@queen.board.should == []
+			end
 		end
-		it "盤の配置（Board）は初期化されているか？" do
-			@queen.board.should == []
-		end
+
 	end
 end

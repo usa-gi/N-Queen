a00514d3c30406cd185181dfb1cb5c7ca0984247
semiya <jun_you@live.jp>
2012-11-05 22:06:44 +0900

    test code is refactored

--- queen_spec.rb
+++ queen_spec.rb
@@ -2,14 +2,14 @@
 require_relative 'queen'
 describe "N-Queen"	do
 	describe "N = 1 のとき"	do
-		it "開始できるか？" do
+		before(:each) do
 			@queen = Queen.new
 			@queen.try(1)
 		end
+		it "クラス名はあっているか？" do
+			@queen.class.should == Queen
+		end
 		it "盤の配置（Board）は初期化されているか？" do
-			@queen = Queen.new
-			@queen.try(1)
-
 			@queen.board.should == []
 		end
 	end

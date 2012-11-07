c98d681dbe839a078ef33188e50814c79496378d
usa-gi <jun_you1985@live.jp>
2012-11-07 23:06:46 +0900

    put_first method 's test is fixed

--- queen_spec.rb
+++ queen_spec.rb
@@ -20,12 +20,12 @@ describe "N-Queen"	do
 					@queen.put_first(0)
 				end
 				it "Qを置くputメソッドを呼び出すか？" do
-					@queen.should_receive(:put).exact(1)
+					@queen.should_receive(:put).exactly(1)
 					@queen.put_first(0)
 				end
 				it "Qは（0,0）におかれるか？" do
 					@queen.put_first(0)
-					@queen.board.should =[0]
+					@queen.board.should == [0]
 				end
 			end
 		end

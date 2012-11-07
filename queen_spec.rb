7d7e79e45dc12906764630d76c9c5a17069aba51
usa-gi <jun_you1985@live.jp>
2012-11-07 22:04:39 +0900

    put_able_check? method 's test is added

--- queen_spec.rb
+++ queen_spec.rb
@@ -19,6 +19,11 @@ describe "N-Queen"	do
 				@queen.put_first(0)
 			end
 		end
+		describe "put_able_check?メソッドを呼んだとき"	do
+			it "一手目のy=0,x=0で置けるか判定するとき、置ける（true）を返すか？" do
+				@queen.put_able_check?(0,0).should be_true
+			end
+		end
 		describe "put_ableメソッドを呼んだとき"	do
 			it "put_ableメソッドは規定回数呼ばれるか？" do
 				@queen.should_receive(:put_able).exactly(1)

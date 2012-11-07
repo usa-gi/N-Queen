631f9cc619cc93ee0cacded8ea18b47ac87032cf
usa-gi <jun_you1985@live.jp>
2012-11-07 22:36:44 +0900

    put_able_check? method  's one statemnt is implemented

--- queen.rb
+++ queen.rb
@@ -1,10 +1,14 @@
 #source file
 class Queen
-	attr_accessor :board
+	attr_accessor :board,:left_slash,:right_slash
 	def initialize(n)
 		@n = n
+
 		@board = Array.new(n)
 		@board.pop # = []
+
+		@left_slash  = [] # /方向
+		@right_slash = [] #\方向
 
 	end
 	def try
@@ -17,7 +21,9 @@ class Queen
 
 	end
 	def put_able_check?(x,y)
-		if @board[y] == nil then
+		if @board[y] == nil \
+		&& @left_slash.index(x+y) == nil then # 左斜め方向にQが存在しない
+#		if @board[y] == nil then
 			return true
 		end
 		false
